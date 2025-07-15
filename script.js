50b177e3ad07d8c050db623e46ee87a48bf619eb
SimoneWin <165174431+SimoneWin@users.noreply.github.com>
2025-07-15 12:15:54 +0200

    Update script.js
    Korrekturen

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const products = [
+products = [
     {
     id: 1,
     name: "Schraube",
-    price: "4.99",
+    price: 4.99,
     image: "",
     },
     {
         id: 2,
         name: "Hammer",
-        price: "29.99",
+        price: 29.99,
         image: "",
     },
     {
@@ -24,15 +24,15 @@ const productList = document.getElementById("product-list");
 products.forEach((product) => {
     const col = document.createElement("div");
     col.className = "col-md-4 mb-4";
-    col.innerHTML = `
-    <div class="card h-100">
-        <img src="${product.image}" class="card-img-top" alt="${product.name}">
-        <div class="card-body d-flex flex-column">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text>${product.price.toFixed(2)}€</p>
-            <button class="btn btn-primary mt-auto" disabled>In den Warenkorb</button>
-        </div>
+    col.innerHTML = 
+  <div class="card h-100">
+    <img src="${product.image}" class="card-img-top" alt="${product.name}">
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">${product.name}</h5>
+      <p class="card-text">${product.price.toFixed(2)} €</p>
+      <button class="btn btn-primary mt-auto" disabled>In den Warenkorb</button>
     </div>
-    `;
+  </div>
+;
     productList.appendChild(col);
 });
